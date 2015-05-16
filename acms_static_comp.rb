892cd8a2d89937d9eeee791b713293f171a17cd5
Ramsey Darling <rdarling42@gmail.com>
2015-05-16 17:33:34 -0400

    working on adding subpages

--- acms_static_comp.rb
+++ acms_static_comp.rb
@@ -5,10 +5,30 @@
 require 'net/http'
 
 def acms_comp(url,src)
+   subpages = []
    uri = URI(url)
-   res = Net::HTTP.get(uri) 
+   res = Net::HTTP.get(uri) #pull down the main page
+   links = URI::extract(res,'http') #grab all the links
+   #grab all linked subpages
+   if links.count > 0
+       links.each do |link|
+            sub_uri = URI(link)
+            subpages << Net::HTTP.get(sub_uri)
+       end
+   end
+   #write main file
    File.open(src,'w') do |f|
       f.write(res) 
+   end
+   #write subpages
+   if subpages.count > 0
+       count = 0
+       subpages.each do |subpage|
+           File.open("subpages/subpage-#{count}.html",'w') do |sf|
+               sf.write(subpage)
+           end
+           count += 1
+       end
    end
 end
 
