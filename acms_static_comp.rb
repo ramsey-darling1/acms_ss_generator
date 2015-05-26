652e0d205e75d6b32dd7c52b798b7495f8975460
Ramsey Darling <rdarling42@gmail.com>
2015-05-25 21:52:11 -0400

    successfully pulling down a wordpress site

--- acms_static_comp.rb
+++ acms_static_comp.rb
@@ -5,18 +5,37 @@
 require 'net/http'
 require 'oga'
 
-def acms_comp(url,src)
-   subpages = []
+def acms_comp_main(url,src)
    uri = URI(url)
    res = Net::HTTP.get(uri) #pull down the main page
    links = URI::extract(res,'http') #grab all the links
+   subpages = grab_subpage_links(links) #pull down the subpages
+   write_subpages(subpages) #write the subpages
+   write_main(res,src) #write the main page
+end
+
+def grab_subpage_links(links)
    #grab all linked subpages
+   subpages = []
    if links.count > 0
        links.each do |link|
-            sub_uri = URI(link)
-            subpages << Net::HTTP.get(sub_uri)
+            if link.length > 7
+                sub_uri = URI(link)
+                subpages << Net::HTTP.get(sub_uri)
+            end
        end
    end
+   return subpages
+end
+
+def write_main(res,src)
+   #write main file
+   File.open(src,'w') do |f|
+      f.write(res) 
+   end
+end
+
+def write_subpages(subpages)
    #write subpages
    if subpages.count > 0
        count = 0
@@ -29,10 +48,6 @@ def acms_comp(url,src)
            count += 1
        end
    end
-   #write main file
-   File.open(src,'w') do |f|
-      f.write(res) 
-   end
 end
 
-acms_comp("http://192.168.33.10/","src/index.html")
+acms_comp_main("http://192.168.33.10/","src/index.html")
